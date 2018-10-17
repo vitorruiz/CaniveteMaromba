f72c2b5dc6c9e6ff80747a5271fff27684054ede
Vitor <vitor.ruiz@evosystems.com.br>
2018-10-17 01:50:40 -0300

    customização do toas

--- app/src/main/java/br/com/vitorruiz/canivetemaromba/toast/MarombaToast.kt
+++ app/src/main/java/br/com/vitorruiz/canivetemaromba/toast/MarombaToast.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.support.v4.content.ContextCompat
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -17,32 +17,64 @@ class MarombaToast(val context: Context, val inflater: LayoutInflater) {
         INFO, SUCCESS, WARNING, ERROR, DEFAULT
     }
 
-    fun showToast(text: String) {
-        val customToastroot = configToast(context, )
-        val customtoast = Toast(context)
-
-        val textView = customToastroot.findViewById<TextView>(R.id.textView1)
-        textView.text = text
-
-        customtoast.view = customToastroot
-        customtoast.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM, 0, 0)
-        customtoast.duration = Toast.LENGTH_LONG
-
-        customtoast.show()
+    fun show(text: String) {
+        configToast(DEFAULT, text).show()
     }
 
-    private fun configToast(context: Context, type: ToastType): View {
-        val customToastroot = inflater.inflate(R.layout.maromba_toast, null)
-        val toastContainer = customToastroot.findViewById<LinearLayout>(R.id.toast_container)
+    fun showSuccess(text: String) {
+        configToast(SUCCESS, text).show()
+    }
+
+    fun showError(text: String) {
+        configToast(ERROR, text).show()
+    }
+
+    fun showInfo(text: String) {
+        configToast(INFO, text).show()
+    }
+
+    fun showWarning(text: String) {
+        configToast(WARNING, text).show()
+    }
+
+    private fun configToast(type: ToastType, text: String): Toast {
+        val customToastRoot = inflater.inflate(R.layout.maromba_toast, null)
+
+        val image = customToastRoot.findViewById<ImageView>(R.id.imageView1)
+        val textView = customToastRoot.findViewById<TextView>(R.id.textView1)
+        val toastContainer = customToastRoot.findViewById<LinearLayout>(R.id.toast_container)
+        val customToast = Toast(context)
 
         when (type) {
-            INFO -> toastContainer.background = ContextCompat.getDrawable(context, R.drawable.info_toast_border)
-            SUCCESS -> toastContainer.background = ContextCompat.getDrawable(context, R.drawable.success_toast_border)
-            WARNING -> toastContainer.background = ContextCompat.getDrawable(context, R.drawable.warning_toast_border)
-            ERROR -> toastContainer.background = ContextCompat.getDrawable(context, R.drawable.error_toast_border)
-            DEFAULT -> toastContainer.background = ContextCompat.getDrawable(context, R.drawable.default_toast_border)
+            INFO -> {
+                toastContainer.background = ContextCompat.getDrawable(context, R.drawable.info_toast_border)
+                image.setImageResource(R.drawable.ic_info)
+            }
+            SUCCESS -> {
+                toastContainer.background = ContextCompat.getDrawable(context, R.drawable.success_toast_border)
+                image.setImageResource(R.drawable.ic_success)
+            }
+            WARNING -> {
+                toastContainer.background = ContextCompat.getDrawable(context, R.drawable.warning_toast_border)
+                image.setImageResource(R.drawable.ic_warning)
+            }
+            ERROR -> {
+                toastContainer.background = ContextCompat.getDrawable(context, R.drawable.error_toast_border)
+                image.setImageResource(R.drawable.ic_error)
+            }
+            DEFAULT -> {
+                toastContainer.background = ContextCompat.getDrawable(context, R.drawable.default_toast_border)
+                image.setImageResource(R.drawable.ic_default)
+            }
         }
 
-        return customToastroot
+
+        textView.text = text
+
+        customToast.view = customToastRoot
+        customToast.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM, 0, 0)
+        customToast.duration = Toast.LENGTH_LONG
+
+        return customToast
     }
 }
